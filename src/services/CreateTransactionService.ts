1928ee264311674e682231b1e020584c4b3dfae6
Wesley Wesleystella Stella <wesleystella@Wesleys-iMac.local>
2020-09-02 19:54:55 -0300

    Finish

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,62 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const categoryRepository = getRepository(Category);
+
+    let findCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You have no balance');
+    }
+
+    if (!findCategory) {
+      findCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(findCategory);
+    }
+
+    const transactionCategory = findCategory;
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
